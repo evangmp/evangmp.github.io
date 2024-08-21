97224fb21a2518ec63d8a5b0386deef8b90c6cd2
evangmp <evangicq@gmail.com>
2024-08-21 11:45:57 +0200

    solve (i hope) type problem with taskAchievement and cookies

--- src/Cookies/Cookies.ts
+++ src/Cookies/Cookies.ts
@@ -1,12 +1,16 @@
-import {ListTask} from "../types/task";
+import {AchievementTable, ListTask} from "../types/task";
 import CookiesConfiguration from "./CookiesConfiguration";
+import task from "../pages/Tasks/Task";
 
 const updateCookie = (listTasks: Array<ListTask>) => {
     for(let i = 0; i < listTasks.length; i++) {
         CookiesConfiguration.setCookie("taskName" + i, listTasks[i].taskName, 30);
         CookiesConfiguration.setCookie("taskDiscipline" + i, listTasks[i].taskDiscipline, 30);
         CookiesConfiguration.setCookie("taskDate" + i, listTasks[i].taskDate, 30);
-        CookiesConfiguration.setCookie("taskAchievement" + i, listTasks[i].taskAchievement, 30);
+        for (let m = 0; i < listTasks[i].taskAchievement.length; m++) {
+            CookiesConfiguration.setCookie("taskDiscipline" + i + m, listTasks[i].taskAchievement[m].toString(), 30);
+        }
+
     }
 };
 
@@ -14,13 +18,17 @@ const getCookie = () => {
     const initializationTasks: Array<ListTask> = [];
     let i:number = 0;
     while(CookiesConfiguration.getCookie("taskName" + i) !== null) {
+        const taskAchievement:  number[] = [];
+        for (let m = 0; m < 11; m++) {
+            taskAchievement.push(Number(CookiesConfiguration.getCookie("taskAchievement"+ i + m)));
+        }
         initializationTasks.push({
             id: 0,
             index: i,
             taskName: CookiesConfiguration.getCookie("taskName" + i).toString(),
             taskDiscipline: CookiesConfiguration.getCookie("taskDiscipline" + i),
             taskAchievement: CookiesConfiguration.getCookie("taskAchievement" + i),
-            taskDate: CookiesConfiguration.getCookie("taskDate" + i).toString(),
+            taskDate: taskAchievement,
         });
         i++
     }
